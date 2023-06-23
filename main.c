d03ab17543a1451c11752ebf5827a4f9a5b9e902
Thet <thetnyihmue12@gmail.com>
2023-06-24 03:06:35 +0630

    Fix Algorithm

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+// Calendar CLI App
+// Author : Thet
+// 24.6.2023
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -7,15 +11,47 @@
 int power(int base, unsigned int p);
 int str_num(const char* num_str);
 int isLeapYear(const int year);
+int year_num(const char* year_str);
+int month_num(const char* month_str);
+int date_num(const int month, const char* date_str);
 void print_date(const int year, const int month, const int date);
 void print_month(const int year, const int month);
 void print_year(const int year);
 
 int main(const int argc, const char* argv[])
 {
-    int year,month,day,date,l_year;
+    int year, month, date;
 
-
+    switch (argc)
+    {
+    case 1:
+        system("echo \x1B[91mInsufficient Arguments\x1B[0m\n");
+        return 0;
+    case 2:
+        year = year_num(argv[1]);
+        if(year == ERR)
+            break;
+        print_year(year);
+        return 0;
+    case 3:
+        year = year_num(argv[1]);
+        month = month_num(argv[2]);
+        if(year == ERR || month == ERR)
+            break;
+        print_month(year, month);
+        return 0;
+    case 4:
+        year = year_num(argv[1]);
+        month = month_num(argv[2]);
+        date = date_num(month, argv[3]);
+        if(year == ERR || month == ERR || date == ERR)
+            break;
+        print_date(year, month, date);
+        return 0;
+    default:
+        break;
+    }
+    system("echo \x1B[91mWrong Arguments\x1B[0m\n");
     return 0;
 }
 
@@ -39,7 +75,7 @@ int str_num(const char* num_str)
     num_dig=i;
     for(i=0;i<num_dig;i++)
     {   if(num_str[i]<48 || num_str[i]>57)
-            return -1;
+            return ERR;
         num += (int)((num_str[i]-48) * power(10,num_dig-i-1));
     }
     return num;
@@ -50,4 +86,63 @@ int isLeapYear(const int year)
     return  (year%400 == 0) ? LEAP :
             (year%100 == 0) ? NOT_LEAP :
             (year%4   == 0) ? LEAP : NOT_LEAP;
+}
+
+int year_num(const char* year_str)
+{
+    int year = str_num(year_str);
+    year = (year < 1800 || year > 3000) ? ERR : year;
+    return  year;
+}
+
+int month_num(const char* month_str)
+{
+    int month = str_num(month_str);
+    if(month != ERR)
+        return (month>12) ? ERR : month - 1;
+
+    for(int i=0; i<NO_MONTH; i++)
+    {
+        if(!strcmp(month_const_str[i],month_str))
+            month = i;
+    }
+    return month;
+}
+
+int date_num(const int month, const char* date_str)
+{
+    int date = str_num(date_str);
+    if(date < 1 || date > 31)
+        date = ERR;
+    else if(month == FEB && date > 29)
+        date = ERR;
+    else if(month == APR || month == JUN || month == SEP || month == NOV)
+    {
+        if(date > 30)
+            date = ERR;
+    }
+    return  date;
+}
+
+void print_date(const int year, const int month, const int date)
+{
+    int leap_flag = isLeapYear(year);
+    int year_cal = (month<2) ? year-1 : year; 
+    int day = (year_cal +(year_cal/4) -(year_cal/100) +(year_cal/400) +month_const[month] +date) % 7;
+    system("echo \x1B[92m");
+    printf("DD\tMM\tYYYY\n");
+    printf("%2d\t%2d\t%4d\t(%s)",date,month+1,year,day_const_str[day]);
+    if(leap_flag == LEAP)
+        printf("\t(Leap year)");
+    system("echo \x1B[0m");
+}
+
+void print_month(const int year, const int month)
+{
+    printf("Later\n");
+}
+
+void print_year(const int year)
+{
+    printf("Later\n");
 }
--- ymd_macros.h
+++ ymd_macros.h
@@ -3,37 +3,38 @@
 #define LOW_BOUND_YEAR 1800
 #define LEAP 100
 #define NOT_LEAP 101
+#define ERR -1
 
-enum month {JAN=0, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC};
+enum month_num {JAN=0, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC};
 
-const char* str_month[] =   {"JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+const char* month_const_str[] =   {"JAN", "FEB", "MAR", "APR", "MAY", "JUN",
                              "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
 
-const int c_month[] = {0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5, 4}; // JAN - DEC, FEB_LeapYear
+const int month_const[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4}; // JAN - DEC, FEB_LeapYear
 
-enum day {SUN=0, MON, TUE, WED, THU, FRI, SAT};
+enum day_num {SUN=0, MON, TUE, WED, THU, FRI, SAT};
 
-const char* str_day[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+const char* day_const_str[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
 
 ///////////////////////////////////////////////////////////////////////////
 //                      Tomohiko Sakamoto's Algorithm                    //
 //                                                                       //
 // day = (year+ (year/4)- (year/100)+ (year/400)+ t[month-1]+ day)% 7    //
+// if(Jan && FEB) year-1                                                 //
 //                                                                       //
-//      Month Const                                                      //
-// t[]=                                                                  //
-// January: 0                                                            //
-// February: 3 (in a regular year) or 4 (in a leap year)                 //
-// March: 3                                                              //
-// April: 6                                                              //
-// May: 1                                                                //
-// June: 4                                                               //
-// July: 6                                                               //
-// August: 2                                                             //
-// September: 5                                                          //
-// October: 0                                                            //
-// November: 3                                                           //
-// December: 5                                                           //
+// t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4}                            //
+// January: 0                                            (31)            //
+// February: 3                                           (28, 29) *      //
+// March: 2                                              (31)            //
+// April: 5                                              (30)     *      //
+// May: 0                                                (31)            //
+// June: 3                                               (30)     *      //
+// July: 5                                               (31)            //
+// August: 1                                             (31)            //
+// September: 4                                          (30)     *      //
+// October: 6                                            (31)            //
+// November: 2                                           (30)     *      //
+// December: 4                                           (31)            //
 //                                                                       //
 // After 1, March, 1800                                                  //
 ///////////////////////////////////////////////////////////////////////////
