a53e15e615aa38cb16422a72f14e909825e6a739
Thet <thetnyihmue12@gmail.com>
2023-07-19 19:30:15 +0630

    Changing months into str_array

--- main.c
+++ main.c
@@ -10,21 +10,26 @@
 
 int power(int base, unsigned int p);
 int str_num(const char* num_str);
+char* num_str(const int num, char* str, const int size);
 int isLeapYear(const int year);
 int year_num(const char* year_str);
 int month_num(const char* month_str);
 int date_num(const int month, const char* date_str);
 void check_h_v(const char* str);
 int calc_day(const int year, const int month, const int date);
+int calc_month(const int year, const int month);
 void print_date(const int year, const int month, const int date);
 void print_month(const int year, const int month);
 void print_year(const int year);
 void print_help(void);
 
+char year_calendar [12][56][5]; // [month] [week (7*8)] [day_print_length]
+
 int main(const int argc, const char* argv[])
 {
     int year, month, date;
-
+    
+    memset(year_calendar,'\0',12*56*5);
     switch (argc)
     {
     case 1:
@@ -85,6 +90,27 @@ int str_num(const char* num_str)
         num += (int)((num_str[i]-48) * power(10,num_dig-i-1));
     }
     return num;
+}
+
+char* num_str(const int num, char* str, const int size)
+{
+    int num_length = 1, tenth = 10, i, j=0;
+    while(num/tenth > 0)
+    {
+        num_length++;
+        tenth *= 10;
+    }
+    if(num_length >= size)
+    {
+        str[0] = '\0';
+        return '\0';
+    }
+    for(i=0,j=power(10,num_length-j-1); i<num_length; i++,j/=10)
+    {
+        str[i] = ((num/j)%10) + 48;
+    }
+    str[i] = '\0';
+    return str;
 }
 
 int isLeapYear(const int year)
@@ -153,6 +179,35 @@ int calc_day(const int year, const int month, const int date)
     return day;
 }
 
+int calc_month(const int year, const int month)
+{
+    char year_str[5], date_str[5];
+    int space = 0;
+    int first_day = calc_day(year, month, 1);
+    num_str(year, year_str, 5);
+    strncpy(year_calendar[month][3], month_const_str[month], 5);
+    strncpy(year_calendar[month][6], year_str, 5);
+    for(int i=0; i<NO_DAY; i++)
+    {
+        strncpy(year_calendar[month][7+i], day_const_str[i], 5);
+    }
+    for(space=0; space<first_day; space++)
+    {
+        strncpy(year_calendar[month][14+space], " ", 5);
+    }
+    for(int day_count=1; day_count<=month_limit[month]; day_count++)
+    {
+        num_str(day_count,date_str,5);
+        strncpy(year_calendar[month][13+space+day_count], date_str, 5);
+    }
+    if(isLeapYear(year) && month==FEB)
+    {
+        num_str(29,date_str,5);
+        strncpy(year_calendar[month][13+space+29], date_str, 5);
+    }
+    return 0;
+}
+
 void print_date(const int year, const int month, const int date)
 {
     int day = calc_day(year, month, date);
@@ -166,34 +221,46 @@ void print_date(const int year, const int month, const int date)
 
 void print_month(const int year, const int month)
 {
-    int space;
-    int first_day = calc_day(year, month, 1);
-    int leap_year = isLeapYear(year);
+    calc_month(year,month);
     system("echo \x1B[92m");
-    printf("%20s%15d\n\n",month_const_str[month],year);
-    for(int i=0; i<NO_DAY; i++)
+    for(int j=0; j<56; j++)
     {
-        printf("%5s",day_const_str[i]);
-    }
-    printf("\n");
-    for(space=0; space<first_day; space++)
-    {
-        printf("     "); // 5 * space 
-    }
-    for(int day_count=1; day_count<=month_limit[month]; day_count++)
-    {
-        printf("%5d",day_count);
-        if(((space+day_count)%7 == 0) && (day_count!=month_limit[month]))
+        printf("%5s",year_calendar[month][j]);
+        if(j!=0 && j%7==6)
             printf("\n");
     }
-    if(leap_year && month==FEB)
-        printf("%5d", 29);
     system("echo \x1B[0m");
 }
 
 void print_year(const int year)
 {
-    printf("Later\n");
+    char year_str[5];
+    num_str(year,year_str,5);
+    int i,j,k;
+    for(int i=0; i<NO_MONTH; i++)
+    {
+        calc_month(year,i);
+    }
+    system("echo \x1B[92m");
+    printf("%60s\n\n",year_str);
+    for(int line=0,i=0; line<32; line++,i++)
+    {   
+        if(i>7)
+        {
+            i = 0;
+            printf("\n");
+        }
+        for(j=0; j<3; j++)
+        {
+            for(k=0; k<7; k++)
+            {
+                printf("%5s",year_calendar[((line/8)*3)+j][(i*7)+k]);
+            }
+            printf("%5s"," ");
+        }
+        printf("\n");
+    }
+    system("echo \x1B[0m");
 }
 
 void print_help(void)
